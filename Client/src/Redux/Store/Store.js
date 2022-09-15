16576346e978a846cfdc778c91e80ec23d8f57c6
hari1538radha <hari.r2020ece@sece.ac.in>
2022-09-15 18:18:35 +0530

    Displaydata slice ,store is been created and updated sucessfully

--- Client/src/Redux/Store/Store.js
+++ Client/src/Redux/Store/Store.js
@@ -2,7 +2,8 @@ import { configureStore ,combineReducers } from "@reduxjs/toolkit";
 
 import loginReducer from "../loginSlice"
 import returndata from "../signupSlice"
-const rootReducer = combineReducers({  User:loginReducer, sigupdata:returndata })
+import Display from "../DisplayData";
+const rootReducer = combineReducers({  User:loginReducer, sigupdata:returndata, Displaydata:Display})
 const store = configureStore({
    
         reducer:rootReducer,
